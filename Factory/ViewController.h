07c7c106b9b5dc7eba5e033a9de9eab1e7b1c333
sos1a2a3a <81995383@qq.com>
2016-05-04 17:08:23 +0800

    Initial Commit

---
+++ Factory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Factory
+//
+//  Created by 李家瑞 on 16/5/4.
+//  Copyright © 2016年 lijiarui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Factory/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Factory
+//
+//  Created by 李家瑞 on 16/5/4.
+//  Copyright © 2016年 lijiarui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
